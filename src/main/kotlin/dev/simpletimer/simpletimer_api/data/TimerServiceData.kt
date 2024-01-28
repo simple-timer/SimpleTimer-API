43d3d7b39eb4f15299f19b4bf2e99dead5d454fe
JanMaki <mutsukimaki@gmail.com>
2024-01-28 20:11:52 +0900

    変更 ナノ秒で扱っていたのをミリ秒に変更

--- src/main/kotlin/dev/simpletimer/simpletimer_api/data/TimerServiceData.kt
+++ src/main/kotlin/dev/simpletimer/simpletimer_api/data/TimerServiceData.kt
@@ -6,7 +6,7 @@ package dev.simpletimer.simpletimer_api.data
  * @property isStarted 始まっているか
  * @property isMove 動いているか
  * @property isFinish 終了しているか
- * @property startNanoTime タイマーが開始した時間
+ * @property startMilliTime タイマーが開始した時間
  * @property adjustTime タイマーの調整値
  * @property stopTime 一時停止した時の時間を保管 調整に使用
  */
@@ -14,7 +14,7 @@ data class TimerServiceData(
     var isStarted: Boolean = false,
     var isMove: Boolean = true,
     var isFinish: Boolean = false,
-    var startNanoTime: Long = 0L,
+    var startMilliTime: Long = 0L,
     var adjustTime: Long = 0L,
     var stopTime: Long = 0L
 )
--- src/main/kotlin/dev/simpletimer/simpletimer_api/database/Transaction.kt
+++ src/main/kotlin/dev/simpletimer/simpletimer_api/database/Transaction.kt
@@ -3,8 +3,10 @@ package dev.simpletimer.simpletimer_api.database
 import dev.simpletimer.simpletimer_api.data.TimerData
 import dev.simpletimer.simpletimer_api.data.TimerServiceData
 import dev.simpletimer.simpletimer_api.database.dummy.GuildMessageChannel
+import dev.simpletimer.simpletimer_api.database.dummy.GuildMessageChannelSerializer
+import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.sql.and
-import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
 /**
@@ -26,9 +28,12 @@ object Transaction {
 
         //SELECT
         transaction {
-            TimerDataTable.select {
+            //チャンネルと終了済みじゃないかを確認
+            TimerDataTable.selectAll().where {         //チャンネルと終了済みじゃないかを確認
                 //チャンネルと終了済みじゃないかを確認
-                TimerDataTable.channel.eq(GuildMessageChannel(channelId)) and TimerDataTable.isFinish.eq(false)
+                TimerDataTable.channel.eq<@Serializable(with = GuildMessageChannelSerializer::class) GuildMessageChannel>(
+                    GuildMessageChannel(channelId)
+                ) and TimerDataTable.isFinish.eq(false)
             }.forEach {
                 //結果用変数に追加
                 result.add(
@@ -42,7 +47,7 @@ object Transaction {
                             it[TimerDataTable.isStarted],
                             it[TimerDataTable.isMove],
                             it[TimerDataTable.isFinish],
-                            it[TimerDataTable.startNanoTime],
+                            it[TimerDataTable.startMilliTime],
                             it[TimerDataTable.adjustTime],
                             it[TimerDataTable.stopTime]
                         )
